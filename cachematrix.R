1eff7d896939212cfe1e14acda91221c26ef5818
smichalak <samuel.michalak@gmail.com>
2017-02-03 16:16:39 +0100

    Final. Tests successful and comments added.

--- cachematrix.R
+++ cachematrix.R
@@ -10,13 +10,15 @@ makeCacheMatrix <- function(x = matrix()) {
     get <- function() x
     setInv <- function(inv) xInv <<- inv
     getInv <- function() xInv
+    
+    # return new defined functions.
     list(set = set,
          get = get,
          setInv = setInv,
          getInv = getInv)
 }
 
-
+# compute the inverse of the matrix, but only if there is no cached version of the matrix.
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     xInv <- x$getInv()
